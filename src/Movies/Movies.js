1383d2f777ff7c692c33236b0c8f960d60d4329e
LegoWalker <wayneseegers@gmail.com>
2023-07-24 20:15:25 +1200

    put localstorage back

--- src/Movies/Movies.js
+++ src/Movies/Movies.js
@@ -33,13 +33,13 @@ const Movies = () => {
         getMovieRequest(searchValue);
     }, [searchValue]);
 
-    // useEffect(() => {
-    //     const MovieFavourites = JSON.parse(
-    //         localStorage.getItem('react-movie-app-favourites')
-    //     );
+    useEffect(() => {
+        const MovieFavourites = JSON.parse(
+            localStorage.getItem('react-movie-app-favourites')
+        );
 
-    //     setFavourites(MovieFavourites);
-    // }, []);
+        setFavourites(MovieFavourites);
+    }, []);
 
     const saveToLocalStorage = (items) => {
         localStorage.setItem('react-movie-app-favourites', JSON.stringify(items));
